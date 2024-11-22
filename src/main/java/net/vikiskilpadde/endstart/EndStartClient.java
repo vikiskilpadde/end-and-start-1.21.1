e710879de6b8f299790780d48a7a16b0c57226b7
vikiskilpadde <vifja007@gmail.com>
2024-11-22 17:03:10 +0100

    Initial commit

---
+++ src/main/java/net/vikiskilpadde/endstart/EndStartClient.java
@@ -0,0 +1,11 @@
+package net.vikiskilpadde.endstart;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EndStartClient implements ClientModInitializer{
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
